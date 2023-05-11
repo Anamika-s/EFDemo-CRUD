adf8d306a8ae51e8539a88a741f29c5469832112
Anamika-s <anamika.sawhney22@gmail.com>
2023-05-11 11:25:38 +0530

    Add project files.

---
+++ EFDemo-CRUD/Context/BatchDbContext.cs
@@ -0,0 +1,30 @@
+﻿using EFDemo_CRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFDemo_CRUD.Context
+{
+    internal class BatchDbContext : DbContext
+    {
+        public BatchDbContext()
+        {
+
+        }
+
+         
+        public BatchDbContext(DbContextOptions<BatchDbContext> options) : base(options) { }
+        public DbSet<Batch> Batches { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                @"data source=ANAMIKA\SQLSERVER;initial catalog=EmpDb;integrated security=true;TrustServerCertificate=True");
+        }
+
+
+
+    }
+}
